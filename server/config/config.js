b138877639cc8b9f0f9bf235d279f5b7d07c8573
Pancake User <coffeepancake2015@gmail.com>
2018-11-04 15:27:44 +0800

    Add config files for environment and test DB

---
+++ server/config/config.js
@@ -0,0 +1,17 @@
+var env = process.env.NODE_ENV || 'development';
+
+console.log('env *******', env);
+
+if(env === 'development'){
+    console.log('setting up dev environment');
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+}else if(env === 'test'){
+    console.log('setting up test environment');
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
+else{
+    console.log('setting up prod environment');
+    process.env.MONGODB_URI = 'mongodb+srv://sucker:sucker@cluster0-ptkdz.mongodb.net/TodoApp?retryWrites=true';
+}
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,11 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser:true});
-mongoose.connect('mongodb+srv://sucker:sucker@cluster0-ptkdz.mongodb.net/TodoApp?retryWrites=true', {useNewUrlParser:true});
+//mongoose.connect('mongodb+srv://sucker:sucker@cluster0-ptkdz.mongodb.net/TodoApp?retryWrites=true', {useNewUrlParser:true});
+
+console.log('mongodb url',process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser:true});
+//
 
 //mongodb+srv://sucker:<PASSWORD>@cluster0-ptkdz.mongodb.net/test?retryWrites=true
 
