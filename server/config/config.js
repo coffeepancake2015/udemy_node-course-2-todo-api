44b826586563360cd15b4fd5f8e0598fbd19c09e
Pancake User <coffeepancake2015@gmail.com>
2018-11-06 01:15:47 +0800

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,18 +1,31 @@
 var env = process.env.NODE_ENV || 'development';
 
-console.log('env *******', env);
+console.log('Environment *******', env);
 
-if(env === 'development'){
-    console.log('setting up dev environment');
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoApp';
-}else if(env === 'test'){
-    console.log('setting up test environment');
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
-    //process.env.MONGODB_URI = 'mongodb+srv://sucker:sucker@cluster0-ptkdz.mongodb.net/TodoAppTest?retryWrites=true';
-}
-else{
-    console.log('setting up prod environment');
-    process.env.MONGODB_URI = 'mongodb+srv://sucker:sucker@cluster0-ptkdz.mongodb.net/TodoApp?retryWrites=true';
-}
+ if(env === 'development' || env === 'test'){
+    var config = require('./config.json');
+    var envConfig = config[env];
+
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+        console.log(process.env[key]);
+    });
+    
+ }
+
+// if(env==='production'){
+//     console.log('setting up prod environment');
+//     process.env.MONGODB_URI = 'mongodb+srv://sucker:sucker@cluster0-ptkdz.mongodb.net/TodoApp?retryWrites=true';
+// } 
+// else if(env === 'test'){
+//     console.log('setting up test environment');
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
+//     //process.env.MONGODB_URI = 'mongodb+srv://sucker:sucker@cluster0-ptkdz.mongodb.net/TodoAppTest?retryWrites=true';
+// }
+// //else if(env === 'development'){
+// else{
+//     console.log('setting up dev environment');
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoApp';
+// }
